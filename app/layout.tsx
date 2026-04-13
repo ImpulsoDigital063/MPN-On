162c90cb38a00b698227cf5b284a7fb68cc00c96
Eduardo Barros <edubchaves5@gmail.com>
2026-04-13 14:49:58 -0300

    add: pixel Utmify na landing MPN-On

--- app/layout.tsx
+++ app/layout.tsx
@@ -138,6 +138,14 @@ export default function RootLayout({
           data-utmify-prevent-subids=""
           strategy="afterInteractive"
         />
+        <Script id="utmify-pixel" strategy="afterInteractive">{`
+          window.pixelId = "69dd2c766866c6e37b394783";
+          var a = document.createElement("script");
+          a.setAttribute("async", "");
+          a.setAttribute("defer", "");
+          a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+          document.head.appendChild(a);
+        `}</Script>
         {GA_ID && (
           <>
             <Script
